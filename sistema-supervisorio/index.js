ed192c2884b4bdacd25d06e95dafcc0e8f123ea6
Jorge <jorgegomesen@gmail.com>
2021-05-26 20:28:34 -0300

    Adaptação para leitura e escrita em arquivo json

--- sistema-supervisorio/index.js
+++ sistema-supervisorio/index.js
@@ -1,78 +1,94 @@
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
 const Express = require('express');
+const fs = require('fs');
 const cors = require('cors')
 const App = Express();
-
 const port = new SerialPort('/dev/ttyACM0', {baudRate: 9600});
 const parser = port.pipe(new Readline({delimiter: '\n'}));
+const FILE_NAME = './data.json';
 
 App.use(cors());
-
-let users_amount = 0;
-let alcool_dispensed_by_date = {};
-let alcool_dispensed_by_day = {};
-let alcool_dispensed_by_hour = {};
 
 port.on('open', () => {
     console.log("Porta serial iniciada.");
 });
 
 parser.on('data', data => {
+    let data_obj = {};
+    let rawdata = fs.readFileSync(FILE_NAME);
+    let current_data = Buffer.byteLength(rawdata) ? JSON.parse(rawdata) : [];
     let date = new Date().toLocaleString('pt-br', {timeZone: 'America/Recife'}).split(' ');
     let day_str = new Date().toLocaleDateString('pt-br', {weekday: 'long'}).split(',')[0];
-    let date_str = date[0];
-    let time_str = date[1];
-    let hour = time_str.split(':')[0];
 
-    users_amount++;
+    data_obj.date = date[0];
+    data_obj.day_str = day_str;
+    data_obj.time = date[1];
 
-    if (!alcool_dispensed_by_date[date_str])
-        alcool_dispensed_by_date[date_str] = [
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        ];
+    current_data.push(data_obj);
 
-    alcool_dispensed_by_date[date_str][parseInt(hour)]++;
+    fs.writeFileSync(FILE_NAME, JSON.stringify(current_data, null, 2));
 
-    if (!alcool_dispensed_by_day[day_str])
-        alcool_dispensed_by_day[day_str] = 0;
-
-    alcool_dispensed_by_day[day_str]++;
-
-    if (!alcool_dispensed_by_hour[hour])
-        alcool_dispensed_by_hour[hour] = 0;
-
-    alcool_dispensed_by_hour[hour]++;
 });
 
 
 App.get('/data', function (req, res) {
+    let rawdata = fs.readFileSync(FILE_NAME);
+    let current_data = Buffer.byteLength(rawdata) ? JSON.parse(rawdata) : [];
+    let users_amount = current_data.length;
+    let alcool_dispensed_by_date = {};
+    let alcool_dispensed_by_day = {};
+    let alcool_dispensed_by_hour = {};
+    let hour = null;
+
+    for (let reg of current_data) {
+        hour = reg.time.split(':')[0];
+
+        if (!alcool_dispensed_by_date[reg.date])
+            alcool_dispensed_by_date[reg.date] = [
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            ];
+
+        alcool_dispensed_by_date[reg.date][parseInt(hour)]++;
+
+        if (!alcool_dispensed_by_day[reg.day_str])
+            alcool_dispensed_by_day[reg.day_str] = 0;
+
+        alcool_dispensed_by_day[reg.day_str]++;
+
+        if (!alcool_dispensed_by_hour[hour])
+            alcool_dispensed_by_hour[hour] = 0;
+
+        alcool_dispensed_by_hour[hour]++;
+    }
+
     let dispensed_by_day = {
-        keys: [],
+        labels: [],
         values: []
     };
     let dispensed_by_hour = {
-        keys: [],
+        labels: [],
         values: []
     };
     let dispensed_by_day_keys = Object.keys(alcool_dispensed_by_day);
     let dispensed_by_hour_keys = Object.keys(alcool_dispensed_by_hour);
 
     for (let key of dispensed_by_day_keys) {
-        dispensed_by_day.keys.push(key);
-        dispensed_by_day.values.push(alcool_dispensed_by_day[key] / users_amount);
+        dispensed_by_day.labels.push(key);
+        dispensed_by_day.values.push(alcool_dispensed_by_day[key] / users_amount * 100);
     }
 
     for (let key of dispensed_by_hour_keys) {
-        dispensed_by_hour.keys.push(key);
-        dispensed_by_hour.values.push(alcool_dispensed_by_hour[key] / users_amount);
+        dispensed_by_hour.labels.push(`${key}:00`);
+        dispensed_by_hour.values.push(alcool_dispensed_by_hour[key] / users_amount * 100);
     }
 
 
     res.json({
         'dispensed_by_date': Object.assign({}, alcool_dispensed_by_date),
         'dispensed_by_hour': dispensed_by_hour,
-        'dispensed_by_day': dispensed_by_day
+        'dispensed_by_day': dispensed_by_day,
+        'total_users': users_amount
     });
 });
 
--- sistema-supervisorio/js/main.js
+++ sistema-supervisorio/js/main.js
@@ -1,7 +1,28 @@
 $(document).ready(async function () {
+    let line_graph_inst = null;
+    let pie_graph_1_inst = null;
+    let pie_graph_2_inst = null;
+    let current_date = new Date().toLocaleString('pt-br', {timeZone: 'America/Recife'}).split(' ')[0];
+
     try {
         let data_response = await getData();
-        console.log(data_response);
+
+        $('#totalUsers').text(data_response.total_users);
+
+        let select_options_keys = Object.keys(data_response.dispensed_by_date);
+
+        for (let key of select_options_keys) {
+            $('select').append(`<option value="${key}">${key}</option>`);
+
+            if (current_date === key)
+                $('select').val(key);
+        }
+
+        $('select').formSelect();
+
+        line_graph_inst = setFirstGraph(line_graph_inst, data_response.dispensed_by_date[current_date]);
+        pie_graph_1_inst = setSecGraph(pie_graph_1_inst, data_response.dispensed_by_day);
+        pie_graph_2_inst = setThirdGraph(pie_graph_2_inst, data_response.dispensed_by_hour);
     } catch (error) {
         alert(error);
     }
@@ -9,4 +30,164 @@ $(document).ready(async function () {
 
 function getData() {
     return $.get('http://localhost:3000/data');
+}
+
+function tooltipCustom(items) {
+    let number_str = items[0].parsed.toFixed(2);
+
+    return `${number_str}%`;
+}
+
+function setFirstGraph(instance, data) {
+    let ctx = document.getElementById('myChart1').getContext('2d');
+
+    if (instance) {
+        return;
+    }
+
+    return new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [
+                '00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00',
+                '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00',
+                '22:00', '23:00'
+            ],
+            datasets: [{
+                label: 'Álcool dispensado',
+                data: data,
+                backgroundColor:['black'],
+                borderColor: '#62CA76',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Fluxo de álcool dispensado por período na data selecionada',
+                }
+            },
+            scales: {
+                y: {
+                    ticks: {
+                        numSteps: 1,
+                        beginAtZero: true
+                    },
+                }
+            }
+        }
+    });
+}
+
+function setSecGraph(instance, data) {
+    let ctx = document.getElementById('myChart2').getContext('2d');
+
+    if (instance) {
+        return;
+    }
+
+    return new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: data.labels,
+            datasets: [{
+                data: data.values,
+                backgroundColor: [
+                    '#62CA76',
+                    '#413FC6',
+                    '#E45851',
+                    '#FDDE69',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    '#62CA76',
+                    '#413FC6',
+                    '#E45851',
+                    '#FDDE69',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            plugins: {
+                tooltip: {
+                    callbacks: {
+                        footer: tooltipCustom,
+                    }
+                },
+                title: {
+                    display: true,
+                    text: 'Fluxo de álcool dispensado por dia',
+                }
+            },
+        }
+    });
+}
+
+function setThirdGraph(instance, data) {
+    let ctx = document.getElementById('myChart3').getContext('2d');
+
+    if (instance) {
+        return;
+    }
+
+    return new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: data.labels,
+            datasets: [{
+                data: data.values,
+                backgroundColor: [
+                    '#62CA76',
+                    '#413FC6',
+                    '#E45851',
+                    '#FDDE69',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    '#62CA76',
+                    '#413FC6',
+                    '#E45851',
+                    '#FDDE69',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            plugins: {
+                tooltip: {
+                    callbacks: {
+                        footer: tooltipCustom,
+                    }
+                },
+                title: {
+                    display: true,
+                    text: 'Fluxo de álcool dispensado por hora',
+                }
+            },
+        }
+    });
 }
